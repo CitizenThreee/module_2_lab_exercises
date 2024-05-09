9e863881d72dc46f02bedc45ec95ca94f4e9c6a9
CitizenThreee <albeisly@gmail.com>
2024-05-09 18:35:33 +1200

    exercise 1 finished

---
+++ misc/Extra-Exercise-1/Task-6/lights.js
@@ -0,0 +1,48 @@
+let pauseDisplay = false;
+
+let lightChild = 0;
+let children = document.getElementById("lights-container").children;
+let childrenCount = document.getElementById("lights-container").childElementCount;
+
+var r = document.querySelector(':root');
+var rs = getComputedStyle(r);
+
+let redOn = rs.getPropertyValue('--light-colour-1-on');
+let redOff = rs.getPropertyValue('--light-colour-1-off');
+let greenOn = rs.getPropertyValue('--light-colour-2-on');
+let greenOff = rs.getPropertyValue('--light-colour-2-off');
+
+let intervalTime = 1000;
+let newInterval = setInterval(NextLight, intervalTime);
+
+function NextLight() {
+    children[lightChild].style['background-color'] = lightChild % 2 == 0 ? redOff : greenOff;
+    lightChild = (lightChild + 1) % childrenCount;
+    children[lightChild].style['background-color'] = lightChild % 2 == 0 ? redOn : greenOn;
+}
+
+function ChangeDelay() {
+    r.style.setProperty('--light-animation-duration', document.getElementById("user-input").value / 1000 + "s");
+}
+
+function ChangeInterval() {
+    clearInterval(newInterval);
+    intervalTime = document.getElementById("user-interval-input").value;
+    newInterval = setInterval(NextLight, intervalTime)
+}
+
+function ToggleLights() {
+    pauseDisplay = !pauseDisplay;
+
+    switch (pauseDisplay) {
+        case false:
+            document.getElementById("pause-button").value = "Stop Display";
+            newInterval = setInterval(NextLight, intervalTime);
+            break;
+        case true:
+            document.getElementById("pause-button").value = "Start Display";
+            children[lightChild].style['background-color'] = lightChild % 2 == 0 ? redOff : greenOff;
+            clearInterval(newInterval);
+            break;
+    }
+}
